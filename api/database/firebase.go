99de6ffb1e3322f201dbae63b4a320b67ff6244e
PhantomDraven <kevinstatua@gmail.com>
2025-02-16 12:34:14 +0100

    feat: add connection to firebaseDB

--- api/database/firebase.go
+++ api/database/firebase.go
@@ -8,10 +8,14 @@ import (
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
+	"firebase.google.com/go/v4/db"
 	"google.golang.org/api/option"
 )
 
-var FirebaseAuth *auth.Client
+var (
+	FirebaseAuth *auth.Client
+	FirestoreDB  *db.Client
+)
 
 // InitializeFirebase sets up the Firebase connection
 func InitializeFirebase() {
@@ -27,18 +31,23 @@ func InitializeFirebase() {
 		opt := option.WithCredentialsFile("firebase/serviceAccountKey.json")
 		app, err = firebase.NewApp(ctx, nil, opt)
 	} else {
-		// Use Firebase Emulator
+		// Use Firestore Emulator
 		app, err = firebase.NewApp(ctx, nil)
 	}
 
 	if err != nil {
-		log.Fatalf("Error initializing Firebase: %v", err)
+		log.Fatalf("Error initializing Firebase %v", err)
 	}
 
 	FirebaseAuth, err = app.Auth(ctx)
 	if err != nil {
-		log.Fatalf("Error initializing Firebase Auth: %v", err)
+		log.Fatalf("Error initializing Firebase Auth %v", err)
 	}
 
-	log.Println("Firebase initialized successfully")
+	FirestoreDB, err = app.Database(ctx)
+	if err != nil {
+		log.Fatalf("Error initializing Firestore %v", err)
+	}
+
+	log.Printf("Firebase initialized successfully")
 }
